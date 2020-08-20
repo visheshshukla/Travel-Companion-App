7825e0c8867d10cd687bdefed4d855cc48f6687c
Vishesh Shukla <engr.visheshshukla@gmail.com>
2020-08-21 05:23:47 +0530

    added database functions

--- store/places-actions.js
+++ store/places-actions.js
@@ -1,6 +1,9 @@
 import * as FileSystem from 'expo-file-system';
 
+import { insertPlace, fetchPlaces } from '../helpers/db';
+
 export const ADD_PLACE = 'ADD_PLACE';
+export const SET_PLACES = 'SET_PLACES';
 
 export const addPlace = (title, image) => {
   return async dispatch => {
@@ -12,11 +15,30 @@ export const addPlace = (title, image) => {
         from: image,
         to: newPath
       });
+      const dbResult = await insertPlace(
+        title,
+        newPath,
+        'Dummy address',
+        15.6,
+        12.3
+      );
+      console.log(dbResult);
+      dispatch({ type: ADD_PLACE, placeData: { id: dbResult.insertId, title: title, image: newPath } });
     } catch (err) {
       console.log(err);
       throw err;
     }
-
-    dispatch({ type: ADD_PLACE, placeData: { title: title, image: newPath } });
   };
+};
+
+export const loadPlaces = () => {
+    return async dispatch => {
+        try {
+            const dbResult = await fetchPlaces();
+            console.log(dbResult);
+            dispatch({ type: SET_PLACES, places: dbResult.rows._array });
+        } catch (err) {
+            throw err;
+        }
+    };
 };
