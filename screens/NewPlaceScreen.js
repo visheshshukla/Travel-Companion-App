ee79a51114313b4baf7fe5b3db65d15da5cc34bc
Vishesh Shukla <engr.visheshshukla@gmail.com>
2020-08-22 02:51:04 +0530

    project complete

--- screens/NewPlaceScreen.js
+++ screens/NewPlaceScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import {
   ScrollView,
   View,
@@ -17,6 +17,7 @@ import LocationPicker from '../components/LocationPicker';
 const NewPlaceScreen = props => {
   const [titleValue, setTitleValue] = useState('');
   const [selectedImage, setSelectedImage] = useState();
+  const [selectedLocation, setSelectedLocation] = useState();
 
   const dispatch = useDispatch();
 
@@ -26,11 +27,15 @@ const NewPlaceScreen = props => {
   };
 
   const imageTakenHandler = imagePath => {
-      setSelectedImage(imagePath);
+    setSelectedImage(imagePath);
   };
 
+  const locationPickedHandler = useCallback(location => {
+    setSelectedLocation(location);
+  }, []);
+
   const savePlaceHandler = () => {
-    dispatch(placesActions.addPlace(titleValue, selectedImage));
+    dispatch(placesActions.addPlace(titleValue, selectedImage, selectedLocation));
     props.navigation.goBack();
   };
 
@@ -44,7 +49,10 @@ const NewPlaceScreen = props => {
           value={titleValue}
         />
         <ImagePicker onImageTaken={imageTakenHandler} />
-        <LocationPicker navigation={props.navigation} />
+        <LocationPicker
+          navigation={props.navigation}
+          onLocationPicked={locationPickedHandler}
+        />
         <Button
           title="Save Place"
           color={Colors.primary}
--- store/places-actions.js
+++ store/places-actions.js
@@ -1,12 +1,30 @@
 import * as FileSystem from 'expo-file-system';
 
 import { insertPlace, fetchPlaces } from '../helpers/db';
+import ENV from '../env';
 
 export const ADD_PLACE = 'ADD_PLACE';
 export const SET_PLACES = 'SET_PLACES';
 
-export const addPlace = (title, image) => {
+export const addPlace = (title, image, location) => {
   return async dispatch => {
+    const response = await fetch(
+      `https://maps.googleapis.com/maps/api/geocode/json?latlng=${
+        location.lat
+      },${location.lng}&key=${ENV.googleApiKey}`
+    );
+
+    if (!response.ok) {
+      throw new Error('Something went wrong!');
+    }
+
+    const resData = await response.json();
+    if (!resData.results) {
+      throw new Error('Something went wrong!');
+    }
+
+    const address = resData.results[0].formatted_address;
+
     const fileName = image.split('/').pop();
     const newPath = FileSystem.documentDirectory + fileName;
 
@@ -18,12 +36,24 @@ export const addPlace = (title, image) => {
       const dbResult = await insertPlace(
         title,
         newPath,
-        'Dummy address',
-        15.6,
-        12.3
+        address,
+        location.lat,
+        location.lng
       );
       console.log(dbResult);
-      dispatch({ type: ADD_PLACE, placeData: { id: dbResult.insertId, title: title, image: newPath } });
+      dispatch({
+        type: ADD_PLACE,
+        placeData: {
+          id: dbResult.insertId,
+          title: title,
+          image: newPath,
+          address: address,
+          coords: {
+            lat: location.lat,
+            lng: location.lng
+          }
+        }
+      });
     } catch (err) {
       console.log(err);
       throw err;
@@ -32,13 +62,13 @@ export const addPlace = (title, image) => {
 };
 
 export const loadPlaces = () => {
-    return async dispatch => {
-        try {
-            const dbResult = await fetchPlaces();
-            console.log(dbResult);
-            dispatch({ type: SET_PLACES, places: dbResult.rows._array });
-        } catch (err) {
-            throw err;
-        }
-    };
+  return async dispatch => {
+    try {
+      const dbResult = await fetchPlaces();
+      console.log(dbResult);
+      dispatch({ type: SET_PLACES, places: dbResult.rows._array });
+    } catch (err) {
+      throw err;
+    }
+  };
 };
